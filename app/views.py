e5ea0a88cd35b5eecc1cda02ee98d1ef510c7fa1
Matthew Mahabeer <mattmahabeer@gmail.com>
2019-03-04 02:23:20 +0000

    final commit

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ This file creates your application.
 """
 
 from app import app, db, login_manager
+from werkzeug.security import check_password_hash
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
@@ -35,8 +36,12 @@ def login():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
+            username = form.username.data
+            password = form.password.data
             # Get the username and password values from the form.
-
+            
+            user = UserProfile.query.filter_by(username=username).first()
+            
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -44,18 +49,30 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user)
+            if user is not None and user.password == password:
+                login_user(user)
+                flash("Login Successful", "Successful")
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash("Unsuccessful Login", "Unsuccessful")
 
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
-
-
+@app.route('/secure_page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
+    
+@app.route('/logout')
+def logout():
+    flash('Logout was succesful')
+    return redirect(url_for('home'))
 
 ###
 # The functions below should be applicable to all Flask apps.
