8c2785a3e56c7e6139e66beaf3e2ba414f498c1c
Matthew Mahabeer <mattmahabeer@gmail.com>
2019-03-19 03:41:41 +0000

    2nd commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+UPLOAD_FOLDER = './app/static/uploads'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:mahabeer@localhost/lab5"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:project1@localhost/project"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
@@ -15,4 +15,6 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 app.config.from_object(__name__)
+filefolder = app.config['UPLOAD_FOLDER']
+app.debug= True
 from app import views
--- app/views.py
+++ app/views.py
@@ -4,13 +4,14 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
-from app import app, db, login_manager
-from werkzeug.security import check_password_hash
-from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, current_user, login_required
-from app.forms import LoginForm
-from app.models import UserProfile
+from app import app, db, filefolder
+from flask import render_template, request, redirect, url_for, flash,session, abort, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from forms import LoginForm
+from models import UserProfile
+from werkzeug.utils import secure_filename
+import os
+import datetime
 
 
 ###
@@ -29,56 +30,40 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
+@app.route('/profile', methods=[ "GET" ,"POST"])
+def profile ():
     form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            username = form.username.data
-            password = form.password.data
-            # Get the username and password values from the form.
-            
-            user = UserProfile.query.filter_by(username=username).first()
-            
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
-
-            # get user id, load into session
-            if user is not None and user.password == password:
-                login_user(user)
-                flash("Login Successful", "Successful")
-                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
-            else:
-                flash("Unsuccessful Login", "Unsuccessful")
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
-    return render_template("login.html", form=form)
-@app.route('/secure_page')
-@login_required
-def secure_page():
-    return render_template('secure_page.html')
-# user_loader callback. This callback is used to reload the user object from
-# the user ID stored in the session
-@login_manager.user_loader
-def load_user(id):
-    return UserProfile.query.get(int(id))
+    if request.method == "POST" and form.validate_on_submit():
+        lastname = form.lastname.data
+        gender = form.gender.data
+        email = form.email.data
+        location = form.location.data
+        now = datetime.datetime.now()
+        firstname = form.firstname.data
+        file = form.upload.data
+        filename = secure_filename(file.filename)
+        biography = form.biography.data
+        user = UserProfile(first_name=firstname, last_name=lastname, gender=gender, email=email, location=location, biography=biography, photo_name=filename, date_created=now)
+        db.session.add(user)
+        db.session.commit()
+        file.save(os.path.join(filefolder, filename))
+        flash('Successfully added.', 'success')
+        return redirect(url_for('profiles')) 
+    return render_template("profile.html", form=form)
     
-@app.route('/logout')
-def logout():
-    flash('Logout was succesful')
-    return redirect(url_for('home'))
-
-###
-# The functions below should be applicable to all Flask apps.
-###
+  
+@app.route("/profiles")
+def profiles():
+    users = UserProfile.query.all()
+    return render_template('profiles.html', users=users)
 
 
+@app.route("/profiles/<filename>")
+def myprofile(filename):
+    user = UserProfile.query.filter_by(id=filename).first()
+    return render_template('myprofile.html', user=user)
+    
+    
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
@@ -101,6 +86,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+
+
 
 
 if __name__ == '__main__':
